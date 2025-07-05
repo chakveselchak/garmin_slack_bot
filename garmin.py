bf03306294537af9c7f3e6cf0a257edb43a3079d
andrey kuznetsov <an.kuznetsov@bpcbt.com>
2025-07-05 09:06:56 +0300

    Added logger

--- garmin.py
+++ garmin.py
@@ -1,10 +1,20 @@
 from garminconnect import Garmin
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,  # 👈 или INFO, если не хочешь много мусора
+    format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+)
+
+logger = logging.getLogger(__name__)
 
 def get_body_battery(email, password):
     try:
         client = Garmin(email, password)
         client.login()
+        logger.info(f"client = {client}")
         data = client.get_body_battery()
+        logger.info(f"get_body_battery = {data}")
         return data.get("bodyBattery", {}).get("value", None)
     except Exception as e:
         print("Garmin error:", e)
--- scheduler.py
+++ scheduler.py
@@ -5,18 +5,29 @@ from models import db, User
 from garmin import get_body_battery
 from slack_api import update_slack_status
 from flask import Flask
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,  # 👈 или INFO, если не хочешь много мусора
+    format='[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+)
+
+logger = logging.getLogger(__name__)
 
 def update_all_users():
     with Flask(__name__).app_context():
         users = User.query.all()
+        logger.info(f"users = {users}"
         for user in users:
             if user.slack_access_token and user.garmin_email and user.garmin_password:
                 battery = get_body_battery(user.garmin_email, user.garmin_password)
+                logger.info(f"battery = {battery}"
                 if battery is not None:
                     update_slack_status(user.slack_access_token, battery)
 
 def start_scheduler():
-    schedule.every(30).minutes.do(update_all_users)
+    schedule.every(1).minutes.do(update_all_users)
+    logger.info(f"tik")
 
     def run():
         while True:
